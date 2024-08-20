9896f1df715350869b474338d5bdb29d4d725c6b
majer.tamas <majer.tamas@mikrum.hu>
2024-08-20 17:48:38 +0200

    testng

--- src/test/java/hu/mikrum/base/BaseApplicationTests.java
+++ src/test/java/hu/mikrum/base/BaseApplicationTests.java
@@ -1,13 +1,15 @@
 package hu.mikrum.base;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 @SpringBootTest
 class BaseApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("works");
+		Assert.assertEquals(1,1);
 	}
 
 }
